16b714690dd467cdda1a9926edebd13abbf0e62e
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-14 11:10:58 +0900

    Feat : 공원 댓글수정 기능 구현

--- park/views.py
+++ park/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from park.models import Park as ParkModel
+from park.models import ParkComment as ParkCommentModel
 
 from park.serializers import ParkDetailSerializer
 from park.serializers import ParkCommentSerializer
@@ -38,4 +39,24 @@ class ParkCommentView(APIView):
             comment_serializer.save()
             return Response(comment_serializer.data, status=status.HTTP_200_OK)
         
+        return Response({"message": "내용을 입력해주세요"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    # 댓글 수정
+    def put(self, request, park_id, comment_id):
+        try: 
+            comment = ParkCommentModel.objects.get(id=comment_id, user=request.user)  
+            
+        except ParkCommentModel.DoesNotExist:
+            return Response({"message": "해당 댓글이 존재하지 않습니다"}, status=status.HTTP_404_NOT_FOUND)      
+                
+        if comment.comment == request.data["comment"]: 
+            return Response({"message": "수정할 내용을 입력해주세요"}, status=status.HTTP_400_BAD_REQUEST) 
+              
+        comment_serializer = ParkCommentSerializer(comment, data=request.data, partial=True)
+        
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            
+            return Response(comment_serializer.data, status=status.HTTP_200_OK)
         return Response({"message": "내용을 입력해주세요"}, status=status.HTTP_400_BAD_REQUEST)
