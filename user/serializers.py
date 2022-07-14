3c6b1e881e583e2689f209bbe44e9297f8f7b812
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 11:43:17 +0900

    Feat : custom validator 및 custom creator 생성
    1. custom validator : 회원가입 시 입력한 값을 기본 validator로 확인하고, 세세한 부분에 대한 컨트롤을 하기 위해 추가함
    2. custom creator : 비밀번호의 경우, 그냥 저장하게 되면 해싱이 되지 않기 때문에 비밀번호 해싱을 위해 적용

--- user/serializers.py
+++ user/serializers.py
@@ -4,6 +4,9 @@ import re
 from user.models import User as UserModel
 
 
+EMAIL = ("@naver.com", "@gmail.com", "@kakao.com", "@daum.net", "@nate.com", "@outlook.com")
+
+
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -14,3 +17,37 @@ class UserSerializer(serializers.ModelSerializer):
             "password": {"write_only": True},
         }
         
+    def validate(self, data):
+        correct_phone = re.compile("(010)-\d{4}-\d{4}")
+        correct_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
+                
+        phone_input = correct_phone.match(data.get("phone", ""))
+        password_input = correct_password.match(data.get("password", ""))
+        
+        if data.get("username"):
+            if not len(data.get("username", "")) >= 6:
+                raise serializers.ValidationError(
+                    detail={"error": "username의 길이는 6자리 이상이어야 합니다."})
+                
+        if not data.get("email", "").endswith(EMAIL):
+            raise serializers.ValidationError(
+                detail={"error": "네이버, 구글, 카카오, 다음, 네이트, 아웃룩 이메일만 가입할 수 있습니다."})
+        
+        if password_input == None:
+            raise serializers.ValidationError(
+                detail={"error": "비밀번호는 8 자리 이상이며 최소 하나 이상의 영문자, 숫자, 특수문자가 필요합니다."})
+        
+        if phone_input == None:
+            raise serializers.ValidationError(
+                detail={"error": "전화번호는 010-0000-0000 형식으로 작성해주시기 바랍니다."})
+        
+        return data
+        
+    def create(self, validated_data):
+        password = validated_data.pop("password", "")
+        
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+        
+        return user
