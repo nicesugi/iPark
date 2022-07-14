387949585fe1e0ae9cc3e9c05dcd65d92aac15c0
GyeongSuKim <rla04722@naver.com>
2022-07-14 14:53:26 +0900

    Feat : serializers, views, url
    Serializer : ArticleSerializer 작성
    
    views : CommunityView 작성
    
    url : 커뮤니티 url 연결

--- ipark/urls.py
+++ ipark/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from community.views import CommunityView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('community/', include("community.urls")),
 ]
