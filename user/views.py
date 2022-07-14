7a66c3eccb86a0e2eab6a1cc04e600ba5d26d657
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 11:44:49 +0900

    Feat : 회원정보 수정 구현

--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
 
+from user.models import User as UserModel
+
 
 class UserView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -18,3 +20,16 @@ class UserView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 회원정보 수정
+    def put(self, request):
+        user = UserModel.objects.get(id=request.user.id)
+        
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
