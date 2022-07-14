fe1dbf383701edbcd8eb635e90669f555036dcb8
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 11:52:27 +0900

    Feat : 회원가입 테스트 진행
    1. region의 경우, 외래키로 받아오는 내용이므로 bulk_create를 사용해 테스트 db를 생성해 주었다.
    2. setUpTestData()를 사용해 생성한 테스트 db가 해당 테스트케이스가 끝날 때까지 유지될 수 있도록 하였다.

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from user.models import Region as RegionModel
+
+
+# 회원가입 테스트
+class UserRegistrationTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.region = RegionModel.objects.bulk_create([RegionModel(region_name="강남구"),
+                                                      RegionModel(region_name="강동구"),
+                                                      RegionModel(region_name="강북구")])
+
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username" : "user10",
+            "password" : "1010abc!",
+            "fullname" : "user10",
+            "email" : "user10@gmail.com",
+            "phone" : "010-1010-1010",
+            "birthday" : "2022-07-13",
+            "region" : 2
+        }
+        
+        response = self.client.post(url, user_data)
+
+        self.assertEqual(response.status_code, 200)
