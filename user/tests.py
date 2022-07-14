993b024b3d80c0b8b10cb555eedd39b90f8445f2
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 13:12:18 +0900

    Feat : 아이디 찾기 테스트 진행

--- user/tests.py
+++ user/tests.py
@@ -91,3 +91,29 @@ class UserInfoModifyDeleteTest(APITestCase):
         response = self.client.delete(url, HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
         
         self.assertEqual(response.data["message"], "회원탈퇴 성공")
+        
+
+# 아이디 찾기 테스트
+class SearchUsernameTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_data = {
+            "username" : "user10",
+            "password" : "1010abc!",
+            "fullname" : "user10",
+            "email" : "user10@gmail.com",
+            "phone" : "010-1010-1010",
+            "birthday" : "2022-07-13",
+        }
+        cls.user = UserModel.objects.create(**user_data)
+        
+    def test_search_username(self):
+        url = reverse("myid_view")
+        data = {
+            "email" : "user10@gmail.com",
+            "phone" : "010-1010-1010"
+        }
+        
+        response = self.client.post(url, data)
+        
+        self.assertEqual(response.data["username"], "user10")
